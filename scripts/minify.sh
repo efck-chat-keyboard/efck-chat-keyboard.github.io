0a3e821ddedaa1396d769c8c736d9efbd45ab549
Kernc <kerncece@gmail.com>
2022-10-12 17:54:48 +0200

    Fix minify.sh on <pre>, e.g. in Faq

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -7,10 +7,10 @@ set -eux
 dir="${1:-output}"
 
 # Consecutive whitespace
-find "$dir" -type f -regex '.*\.\(html\|js\|css\)$' -exec sed -i -E -e 's/^\s+//g' \
-                                                                    -e 's/\s+/ /g' {} \;
+find "$dir" -type f -regex '.*\.\(js\|css\)$' -exec sed -i -E -e 's/^\s+//g' \
+                                                              -e 's/\s+/ /g' {} \;
 # Consecutive line breaks
-find "$dir" -type f -regex '.*\.\(html\|js\|css\)$' -exec perl -i -p0e 's,\n+\s*,\n,g' {} \;
+find "$dir" -type f -regex '.*\.\(js\|css\)$' -exec perl -i -p0e 's,\n+\s*,\n,g' {} \;
 
 # DANGER: Comments
 find "$dir" -type f -regex '.*\.html$' -exec perl -i -p0e 's/<!--.*?-->//msg' {} \;
@@ -21,8 +21,8 @@ find "$dir" -type f -regex '.*\.css$' -exec sed -i -E 's,/\*.*?\*/,,g' {} \;
 find "$dir" -type f -regex '.*\.html$' -exec perl -i -pe 's,(?s)<(pre|textarea|script)\b[^<]*>.*?<\/\1>(*SKIP)(*FAIL)|([\w:-]+=)"([\w.#%/-]+)"(\s*/(?=>))?,\2\3,g' {} \;
 find "$dir" -type f -regex '.*\.html$' -exec perl -i -pe 's,(?s)<(pre|textarea|script)\b[^<]*>.*?<\/\1>(*SKIP)(*FAIL)|(?<=\w)=""(?=\s),,g' {} \;
 
-# DANGER: Collapse lines, but not inside <pre> or <textarea>
-find "$dir" -type f -regex '.*\.html$' -exec perl -i -p0e 's,(?s)<(pre|textarea)\b[^<]*>.*?<\/\1>(*SKIP)(*FAIL)|\n+\s*, ,g' {} \;
+# DANGER: Collapse whitespace, but not inside <pre> or <textarea>
+find "$dir" -type f -regex '.*\.html$' -exec perl -i -p0e 's,(?s)<(pre|textarea)\b[^<]*>.*?<\/\1>(*SKIP)(*FAIL)|[\n\s]+, ,g' {} \;
 
 # Redundant CSS semicolons
 find "$dir" -type f -regex '.*\.css$' -exec perl -i -p0e 's,;\s*(?=[;}])|,,g' {} \;
